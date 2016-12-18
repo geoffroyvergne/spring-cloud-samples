0500d93bafda42e784ab3718b6f73a3c469fff30
Geoffroy Vergne <geoffroy.vergne@gmail.com>
2016-12-18 12:28:20 +0100

    add sso server

---
+++ sso-server/src/main/java/com/base/Application.java
@@ -0,0 +1,10 @@
+package com.base;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class Application {
+
+}
