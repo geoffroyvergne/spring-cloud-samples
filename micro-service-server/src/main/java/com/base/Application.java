c0839d895023b02b142d03135d87145c8dcc94a9
Geoffroy Vergne <geoffroy.vergne@gmail.com>
2017-04-09 19:51:16 +0200

    updates

--- micro-service-server/src/main/java/com/base/Application.java
+++ micro-service-server/src/main/java/com/base/Application.java
@@ -2,14 +2,17 @@ package com.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableHystrix
 @EnableHystrixDashboard
+@EnableTurbine
 @EnableZuulProxy
 public class Application {
     public static void main(String[] args) throws Exception {
